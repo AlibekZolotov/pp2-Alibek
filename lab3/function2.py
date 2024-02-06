561a962f83f4bd5e1eb86b817f9381e7a7aa5efa
Alibek <alibekberik13@gmail.com>
2024-02-06 22:26:12 +0600

    yyy

--- lab3/function2.py
+++ lab3/function2.py
@@ -1,83 +1,7 @@
 #ex1
-
+from dict_of_movies import movies
 def is_above_5_5(movies,movie_name):
-    movies = [
-{
-"name": "Usual Suspects", 
-"imdb": 7.0,
-"category": "Thriller"
-},
-{
-"name": "Hitman",
-"imdb": 6.3,
-"category": "Action"
-},
-{
-"name": "Dark Knight",
-"imdb": 9.0,
-"category": "Adventure"
-},
-{
-"name": "The Help",
-"imdb": 8.0,
-"category": "Drama"
-},
-{
-"name": "The Choice",
-"imdb": 6.2,
-"category": "Romance"
-},
-{
-"name": "Colonia",
-"imdb": 7.4,
-"category": "Romance"
-},
-{
-"name": "Love",
-"imdb": 6.0,
-"category": "Romance"
-},
-{
-"name": "Bride Wars",
-"imdb": 5.4,
-"category": "Romance"
-},
-{
-"name": "AlphaJet",
-"imdb": 3.2,
-"category": "War"
-},
-{
-"name": "Ringing Crime",
-"imdb": 4.0,
-"category": "Crime"
-},
-{
-"name": "Joking muck",
-"imdb": 7.2,
-"category": "Comedy"
-},
-{
-"name": "What is the name",
-"imdb": 9.2,
-"category": "Suspense"
-},
-{
-"name": "Detective",
-"imdb": 7.0,
-"category": "Suspense"
-},
-{
-"name": "Exam",
-"imdb": 4.2,
-"category": "Thriller"
-},
-{
-"name": "We Two",
-"imdb": 7.2,
-"category": "Romance"
-}
-]
+    
     for movie in movies:
         if movie["name"].lower() == movie_name.lower():
             return movie["imdb"] > 5.5
@@ -86,84 +10,9 @@ user_input = input("Enter the name of the movie")
 result = is_above_5_5(movies,user_input)
 print(result)
 #ex2
+from dict_of_movies import movies
 def filter_above(movies):
-    movies = [
-{
-"name": "Usual Suspects", 
-"imdb": 7.0,
-"category": "Thriller"
-},
-{
-"name": "Hitman",
-"imdb": 6.3,
-"category": "Action"
-},
-{
-"name": "Dark Knight",
-"imdb": 9.0,
-"category": "Adventure"
-},
-{
-"name": "The Help",
-"imdb": 8.0,
-"category": "Drama"
-},
-{
-"name": "The Choice",
-"imdb": 6.2,
-"category": "Romance"
-},
-{
-"name": "Colonia",
-"imdb": 7.4,
-"category": "Romance"
-},
-{
-"name": "Love",
-"imdb": 6.0,
-"category": "Romance"
-},
-{
-"name": "Bride Wars",
-"imdb": 5.4,
-"category": "Romance"
-},
-{
-"name": "AlphaJet",
-"imdb": 3.2,
-"category": "War"
-},
-{
-"name": "Ringing Crime",
-"imdb": 4.0,
-"category": "Crime"
-},
-{
-"name": "Joking muck",
-"imdb": 7.2,
-"category": "Comedy"
-},
-{
-"name": "What is the name",
-"imdb": 9.2,
-"category": "Suspense"
-},
-{
-"name": "Detective",
-"imdb": 7.0,
-"category": "Suspense"
-},
-{
-"name": "Exam",
-"imdb": 4.2,
-"category": "Thriller"
-},
-{
-"name": "We Two",
-"imdb": 7.2,
-"category": "Romance"
-}
-]
+    
     return[movie for movie in movies if movie["imdb"] > 5.5]
    
         
@@ -171,85 +20,10 @@ def filter_above(movies):
 filtered_movies = filter_above(movies)
 print(filtered_movies)
 #ex3
+from dici_of_movies import movies
 def filter_movies (movies,category):
     return[movie for movie in movies if movie["category"] == category]
-movies = [
-{
-"name": "Usual Suspects", 
-"imdb": 7.0,
-"category": "Thriller"
-},
-{
-"name": "Hitman",
-"imdb": 6.3,
-"category": "Action"
-},
-{
-"name": "Dark Knight",
-"imdb": 9.0,
-"category": "Adventure"
-},
-{
-"name": "The Help",
-"imdb": 8.0,
-"category": "Drama"
-},
-{
-"name": "The Choice",
-"imdb": 6.2,
-"category": "Romance"
-},
-{
-"name": "Colonia",
-"imdb": 7.4,
-"category": "Romance"
-},
-{
-"name": "Love",
-"imdb": 6.0,
-"category": "Romance"
-},
-{
-"name": "Bride Wars",
-"imdb": 5.4,
-"category": "Romance"
-},
-{
-"name": "AlphaJet",
-"imdb": 3.2,
-"category": "War"
-},
-{
-"name": "Ringing Crime",
-"imdb": 4.0,
-"category": "Crime"
-},
-{
-"name": "Joking muck",
-"imdb": 7.2,
-"category": "Comedy"
-},
-{
-"name": "What is the name",
-"imdb": 9.2,
-"category": "Suspense"
-},
-{
-"name": "Detective",
-"imdb": 7.0,
-"category": "Suspense"
-},
-{
-"name": "Exam",
-"imdb": 4.2,
-"category": "Thriller"
-},
-{
-"name": "We Two",
-"imdb": 7.2,
-"category": "Romance"
-}
-]
+
 category_name = input("Enter the name of category")
 movies_in = filter_movies(movies, category_name)
 if movies_in:
@@ -257,174 +31,24 @@ if movies_in:
 else:
     print("No,they are not the same")
 #ex4
+from dict_of_movies import movies
 def calculate_average_imdb(movies):
     if not movies:
         return 0.0
     total_imdb = sum(movie["imdb"] for movie in movies)
     average_imdb = total_imdb / len(movies)
     return average_imdb
-movies = [
-{
-"name": "Usual Suspects", 
-"imdb": 7.0,
-"category": "Thriller"
-},
-{
-"name": "Hitman",
-"imdb": 6.3,
-"category": "Action"
-},
-{
-"name": "Dark Knight",
-"imdb": 9.0,
-"category": "Adventure"
-},
-{
-"name": "The Help",
-"imdb": 8.0,
-"category": "Drama"
-},
-{
-"name": "The Choice",
-"imdb": 6.2,
-"category": "Romance"
-},
-{
-"name": "Colonia",
-"imdb": 7.4,
-"category": "Romance"
-},
-{
-"name": "Love",
-"imdb": 6.0,
-"category": "Romance"
-},
-{
-"name": "Bride Wars",
-"imdb": 5.4,
-"category": "Romance"
-},
-{
-"name": "AlphaJet",
-"imdb": 3.2,
-"category": "War"
-},
-{
-"name": "Ringing Crime",
-"imdb": 4.0,
-"category": "Crime"
-},
-{
-"name": "Joking muck",
-"imdb": 7.2,
-"category": "Comedy"
-},
-{
-"name": "What is the name",
-"imdb": 9.2,
-"category": "Suspense"
-},
-{
-"name": "Detective",
-"imdb": 7.0,
-"category": "Suspense"
-},
-{
-"name": "Exam",
-"imdb": 4.2,
-"category": "Thriller"
-},
-{
-"name": "We Two",
-"imdb": 7.2,
-"category": "Romance"
-}
-]
+
 average_score = calculate_average_imdb(movies)
 print(f"The average IMDB score of the movies is: {average_score}")
 #ex5
+from dict_of_movies import movies
 def calculate_average_imdb_by_category(movies, category):
     category_movies = [movie for movie in movies if movie["category"] == category]
     total_imdb = sum(movie["imdb"] for movie in category_movies)
     average_imdb = total_imdb / len(category_movies)
     return average_imdb
-movies = [
-{
-"name": "Usual Suspects", 
-"imdb": 7.0,
-"category": "Thriller"
-},
-{
-"name": "Hitman",
-"imdb": 6.3,
-"category": "Action"
-},
-{
-"name": "Dark Knight",
-"imdb": 9.0,
-"category": "Adventure"
-},
-{
-"name": "The Help",
-"imdb": 8.0,
-"category": "Drama"
-},
-{
-"name": "The Choice",
-"imdb": 6.2,
-"category": "Romance"
-},
-{
-"name": "Colonia",
-"imdb": 7.4,
-"category": "Romance"
-},
-{
-"name": "Love",
-"imdb": 6.0,
-"category": "Romance"
-},
-{
-"name": "Bride Wars",
-"imdb": 5.4,
-"category": "Romance"
-},
-{
-"name": "AlphaJet",
-"imdb": 3.2,
-"category": "War"
-},
-{
-"name": "Ringing Crime",
-"imdb": 4.0,
-"category": "Crime"
-},
-{
-"name": "Joking muck",
-"imdb": 7.2,
-"category": "Comedy"
-},
-{
-"name": "What is the name",
-"imdb": 9.2,
-"category": "Suspense"
-},
-{
-"name": "Detective",
-"imdb": 7.0,
-"category": "Suspense"
-},
-{
-"name": "Exam",
-"imdb": 4.2,
-"category": "Thriller"
-},
-{
-"name": "We Two",
-"imdb": 7.2,
-"category": "Romance"
-}
-]
+
 category_name = input("Enter the name of the category: ")
 average_score = calculate_average_imdb_by_category(movies, category_name)
 print(average_score)
